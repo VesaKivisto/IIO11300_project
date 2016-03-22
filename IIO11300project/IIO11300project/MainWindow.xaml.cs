ecf2485984bcaeabb72fecfe9dab01e8e285cc0e
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-03-22 22:23:35 +0200

    Started doing the project. Worked out how to parse JSON with C#

---
+++ IIO11300project/IIO11300project/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IIO11300project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitCombobox();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            RiotApiHandler apiHandler = new RiotApiHandler();
+            string region = cbRegions.SelectedValue.ToString();
+            string summonername = txtSummonername.Text;
+            apiHandler.RequestSummonerData(region, summonername);
+        }
+
+        private void InitCombobox()
+        {
+            string[] regions = new string[10] { "BR", "EUNE", "EUW", "KR", "LAN", "LAS", "NA", "OCE", "RU", "TR" };
+            cbRegions.ItemsSource = regions;
+        }
+    }
+}
---
+++ IIO11300project/IIO11300project/RiotApiHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Web.Helpers;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IIO11300project
+{
+    class RiotApiHandler
+    {
+        private string apiKey;
+        private string region;
+        private string summonername;
+
+        public RiotApiHandler()
+        {
+            this.apiKey = "b4772fd0-7fdc-4b50-a7bd-e9b4a2c8f61d";
+        }
+
+        public void RequestSummonerData(string region, string summonername)
+        {
+            this.region = region.ToLower();
+            this.summonername = summonername.ToLower();
+            string url = "https://" + this.region + ".api.pvp.net/api/lol/" + this.region + "/v1.4/summoner/by-name/" + this.summonername + "?api_key=" + this.apiKey;
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            string json = client.DownloadString(url);
+
+            //SummonerData summoner = JsonConvert.DeserializeObject<SummonerData>(json);
+            //MessageBox.Show(summoner.troikku.Name);
+
+            // TÄMÄ TOIMII, WAUUUUUU
+            JObject o = JObject.Parse(json);
+            string id = o[this.summonername]["id"].ToString();
+            string name = o[this.summonername]["name"].ToString();
+            string profileIcon = o[this.summonername]["profileIconId"].ToString();
+
+            MessageBox.Show("ID: " + id + "\n\rName: " + name + "\n\rIcon: " + profileIcon);
+        }
+    }
+}
