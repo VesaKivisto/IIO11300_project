c3e951e8263789e9951828df8507f4013ea1254b
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-03-31 15:20:37 +0300

    Fixed the repository. Tried to revert an earlier commit but accidentally broke the whole thing.

--- IIO11300project/IIO11300project/RiotApiHandler.cs
+++ IIO11300project/IIO11300project/RiotApiHandler.cs
@@ -160,7 +160,7 @@ namespace IIO11300project
         public string GetPlatformByRegion(string region)
         {
             Dictionary<string, string> regions = GetRegionsPlatforms();
-            string platformID = regions[region.ToLower()];
+            string platformID = regions[region.ToUpper()];
             return platformID;
         }
 
